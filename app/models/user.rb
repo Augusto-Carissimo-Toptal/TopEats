8535e9b60517fea19a6bab9f543b57728422fa61
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-01-04 12:13:16 -0300

    Add enum validation to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  has_many :orders
-  has_one :adress
-
+  enum type: { costumer: 'costumer', courier: 'courier' }
+  
   validates :full_name, presence: true
   validates :phone_number, presence: true
   validates :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  
-  enum type: { costumer: 'costumer', courier: 'courier' }
+   
+  has_many :orders
+  has_one :adress
 end
