73314d20c74c953aaefbd18e52655b4e57d6150d
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-01-05 18:18:21 -0300

    Add validation type in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
   validates :phone_number, presence: true
   validates :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  # validates :type, { inclusion: { in: %w[Customer Courier] } } Existe algo de este estilo
+  validates :type, inclusion: { in: %w(Customer Courier)}
 
-  # has_many :orders
   has_one :user_address
   has_one :address, through: :user_address
 end
