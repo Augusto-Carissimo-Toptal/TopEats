c9b9daa2da4eaea23d2dc3bc17f0d856c71dd011
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-01-05 08:53:35 -0300

    Add total_price validation to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,9 @@
 class Order < ApplicationRecord
-  enum status: { pending: 'pending', accepted: 'accepted', preparing: 'preparing',delivered: 'delivered' }
+  enum status: { pending: 'pending', accepted: 'accepted', preparing: 'preparing', delivered: 'delivered' }
+  validates :total_price, presence: true
 
   belongs_to :courier, class_name: 'User'
   belongs_to :customer, class_name: 'User'
-  has_many :menu_items
+  has_many :order_menu
+  has_many :menu_items, through: :order_menu 
 end
