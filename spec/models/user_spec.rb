e01c83f032f26cbabce892a89e59be5364f7fd27
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-01-05 18:21:09 -0300

    Delete have one :address

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,11 +6,9 @@ RSpec.describe User do
     it { should validate_presence_of(:phone_number) }
     it { should validate_presence_of(:email) }
     it { should allow_value('augusto@email.com').for(:email)}
-    # it { should define_enum_for(:type).with(customer: 'customer', courier: 'courier').backed_by_column_of_type(:string) }
    end
 
   context 'associations' do
-    # it { should have_many(:orders) }
     it { should have_one(:address) }
   end
 end
