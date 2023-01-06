eb209ba19242383b882083cedb84c520e26bf0c1
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-01-06 16:53:53 -0300

    Add CustomerController routes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,7 @@
 class CustomersController < ApplicationController
 
+  before_action :set_customer
+  
   def index 
     @customers = Customer.all
     
@@ -11,12 +13,54 @@ class CustomersController < ApplicationController
     render json: @customer
   end
   
-  def destroy
-    @customer = Customer.find(params[:id])
-    @customer.destroy
-    render json: Customer.all
+  def new
+    @customer = Customer.new
   end
 
-  
+  def edit
+  end
 
+  def create
+    @customer = Customer.new(customer_params)
+
+    respond_to do |format|
+      if @customer.save
+        format.html { redirect_to customer_url(@customer), notice: "Customer was successfully created." }
+        format.json { render :show, status: :created, location: @customer }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @customer.update(customer_params)
+        format.html { redirect_to customer_url(@customer), notice: "Customer was successfully updated." }
+        format.json { render :show, status: :ok, location: @customer }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @customer.destroy
+
+    respond_to do |format|
+      format.html { redirect_to widgets_url, notice: "Customer was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_customer
+      @customer = Customer.find(params[:id])
+    end
+  
+    def customer_params
+      params.require(:customer).permit(:full_name,:phone_number, :email)
+    end
 end
---
+++ spec/controllers/customer_controllers_spec.rb
@@ -0,0 +1,118 @@
+require "rails_helper"
+
+RSpec.describe CustomersController, type: :request do
+  let(:valid_attributes) {
+    {full_name: 'new user', phone_number: '1122334455', email: 'destroy@toptal.com'}
+  }
+
+  let(:invalid_attributes) {
+    {full_name:'', phone_number: '', email: ''}
+  }
+
+  context "GET /index" do
+    it "renders a successful response" do
+      Customer.create! valid_attributes
+      get customers_url
+      expect(response).to be_successful
+    end
+  end
+
+  context "GET /show" do
+    it "renders a successful response" do
+      customer = Customer.create! valid_attributes
+      get customer_url(customer)
+      expect(response).to be_successful
+    end
+  end
+
+  context "GET /new" do
+    it "renders a successful response" do
+      get new_customer_url
+      expect(response).to be_successful
+    end
+  end
+
+  context "GET /edit" do
+    it "renders a successful response" do
+      customer = Customer.create! valid_attributes
+      get edit_customer_url(customer)
+      expect(response).to be_successful
+    end
+  end
+
+  context "POST /create" do
+    context "with valid parameters" do
+      it "creates a new Customer" do
+        expect {
+          post customers_url, params: { customer: valid_attributes }
+        }.to change(Customer, :count).by(1)
+      end
+
+      it "redirects to the created customer" do
+        post customers_url, params: { customer: valid_attributes }
+        expect(response).to redirect_to(customer_url(Customer.last))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new Customer" do
+        expect {
+          post customers_url, params: { custome: invalid_attributes }
+        }.to change(Customer, :count).by(0)
+      end
+
+      it "renders a response with 422 status (i.e. to display the 'new' template)" do
+        post customers_url, params: { customer: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  context "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) {
+        {full_name: 'new user', phone_number: '1122334455', email: 'destroy@toptal.com'}
+      }
+
+      it "updates the requested customer" do
+        customer = Customer.create! valid_attributes
+        patch customer_url(customer), params: { customer: new_attributes }
+        customer.reload
+        skip("Add assertions for updated state")
+      end
+
+      it "redirects to the customer" do
+        customer = Customer.create! valid_attributes
+        patch customer_url(customer), params: { customer: new_attributes }
+        customer.reload
+        expect(response).to redirect_to(customer_url(customer))
+      end
+    end
+
+    context "with invalid parameters" do
+    
+      it "renders a response with 422 status (i.e. to display the 'edit' template)" do
+        customer = Customer.create! valid_attributes
+        patch customer_url(customer), params: { customer: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it "destroys the requested customer" do
+      customer = Customer.create! valid_attributes
+      expect {
+        delete customer_url(customer)
+      }.to change(Customer, :count).by(-1)
+    end
+
+    it "redirects to the customers list" do
+      customer = Customer.create! valid_attributes
+      delete customer_url(customer)
+      expect(response).to redirect_to(customer_url)
+    end
+  end
+
+end
