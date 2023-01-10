d074bf2dd4e214101ab88f6dc9cfe4682d5bdd2d
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-01-10 16:11:02 -0300

    Refactor Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,8 @@ class Order < ApplicationRecord
   enum status: { pending: 'pending', accepted: 'accepted', preparing: 'preparing', delivered: 'delivered' }
   validates :total_price, presence: true, numericality: {only_float: true}
 
-  belongs_to :courier, class_name: 'User'
-  belongs_to :customer, class_name: 'User'
+  belongs_to :courier, class_name: 'User', foreign_key: 'courier_id'
+  belongs_to :customer, class_name: 'User', foreign_key: 'customer_id'
   has_many :order_menu
   has_many :menu_items, through: :order_menu 
 end
