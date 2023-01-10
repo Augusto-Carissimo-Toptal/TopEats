90d4e45cd3b506dfd08e98264c01aa213779a80b
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2023-01-10 09:30:15 -0300

    Refactor phone_number validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :full_name, presence: true
-  validates :phone_number, presence: true
+  validates_format_of :phone_number, with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
+  message: "- Phone numbers must be in xxx-xxx-xxxx format."
   validates :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :type, inclusion: { in: %w(Customer Courier)}
